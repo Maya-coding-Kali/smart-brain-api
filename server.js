5dc28f1f452da9ffe45d3f4104040443ea89aa28
Maya <mayasophiakali@gmail.outlook.com>
2022-06-28 15:43:50 -0700

    update bcrypt

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ const host = '0.0.0.0';
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "root",
-    database: "smart-brain",
-  },
+    connectionString: process.env.DATABASE_URL,
+    ssl:{
+      rejectUnauthorized: false,
+    }
+  }
 });
 const app = express();
 app.use(express.json());
