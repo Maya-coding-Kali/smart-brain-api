4c4f8673434ef05a66ae80b2748f7463089507ac
Maya <mayasophiakali@gmail.outlook.com>
2022-06-28 23:38:12 -0700

    update connection string

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,6 @@
 const handelRegister = (req, res, db, bcrypt) => {  
     const { email, name, password } = req.body;
+    console.log(email, name, password)
     if (!email || !name || !password) {
     return    res.status(400).json("incorrect form submission")
     }
