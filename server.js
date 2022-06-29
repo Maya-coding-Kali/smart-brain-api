cc755e6d929f0b1cd98b6fa87653c5d243e78692
Maya <mayasophiakali@gmail.outlook.com>
2022-06-28 18:47:40 -0700

    update knex

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {res.send('it is working')});
 
 app.post("/signin", (req, res) => {
   signIn.handelSignIn(req, res, db, bcrypt);
