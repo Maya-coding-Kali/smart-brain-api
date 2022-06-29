17949876a0956f49662248096e24286e2555f717
Maya <mayasophiakali@gmail.outlook.com>
2022-06-28 23:42:35 -0700

    update connection string

--- controllers/register.js
+++ controllers/register.js
@@ -2,7 +2,7 @@ const handelRegister = (req, res, db, bcrypt) => {
     const { email, name, password } = req.body;
     console.log(email, name, password)
     if (!email || !name || !password) {
-    return    res.status(400).json("incorrect form submission " + email, name, password)
+    return    res.status(400).json("incorrect form submission " + email + " " + name +" " + password)
     }
     const saltRounds = 10;
     const salt = bcrypt.genSaltSync(saltRounds);
