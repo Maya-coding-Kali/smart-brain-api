7024aee81d44e887bc316dd66ce357fb531e59aa
Maya <mayasophiakali@gmail.outlook.com>
2022-06-30 19:17:17 -0700

    update celebrity model

--- controllers/image.js
+++ controllers/image.js
@@ -4,7 +4,7 @@ const app = new Clarifai.App({
 });
 const handelApi = (req, res) => {
   app.models
-    .predict(Clarifai.CELEBRITY_MODEL, req.body.input)
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then((data) => {
       res.json(data);
     })
